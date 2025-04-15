f1a6962201b51d30a78468dd4e5f724b411df61e
youness anflous <anayouness51@gmail.com>
2025-04-15 14:26:04 +0100

    mac_addr_changer v1.0.0

--- mac_addr_changer.py
+++ mac_addr_changer.py
@@ -24,12 +24,22 @@ import	sys
 def color(text, color_code):
 	return f"\033[{color_code}m{text}\033[0m"
 
+def	check_interface_exists(interface):
+	return os.path.exists(f"/sys/class/net/{interface}")
+
+def	check_valid_interface(interface):
+	return bool(re.match(r'^[a-zA-Z][a-zA-Z0-9_-]{0,14}$', interface))
+
+def	check_misconfigured_mac(user_mac_addr):
+    first_byte = int(user_mac_addr.split(":")[0], 16)
+    return not (first_byte & 1)
+
 def	setup_new_addr(user_mac_addr, interface):
 	try:
-		subprocess.run(["sudo", "ip", "link", "set", "dev", interface, "down"], check=True)
-		subprocess.run(["sudo", "ip", "link", "set", "dev", interface, "address", user_mac_addr], check=True)
-		subprocess.run(["sudo", "ip", "link", "set", "dev", interface, "up"], check=True)
-		print(color("[+] MAC address is changed succassfully", "92"))
+		subprocess.run(["ip", "link", "set", "dev", interface, "down"], check=True)
+		subprocess.run(["ip", "link", "set", "dev", interface, "address", user_mac_addr], check=True)
+		subprocess.run(["ip", "link", "set", "dev", interface, "up"], check=True)
+		print(color("[+] MAC address is changed successfully", "92"))
 	except subprocess.CalledProcessError as error:
 		print(color(f"[!] Error changing MAC address: {error}", "31"))
 		sys.exit(1)
@@ -56,10 +66,22 @@ def	user_args():
 def	main():
 	check_if_root()
 	args = user_args()
+	
+	if not check_interface_exists(args.interface):
+		print(color(f"[!] Interface {args.interface} not exists.", "31"))
+		sys.exit(1)
+	
+	if not check_valid_interface(args.interface):
+		print(color(f"[!] Invalid interface name.", "31"))
+		sys.exit(1)
+	if not check_misconfigured_mac(args.mac_addr):
+		print(color("[!] Invalid MAC address -> (must be unicast).", "31"))
+		sys.exit(1)
+
 	if parsing_input(args.mac_addr):
 		setup_new_addr(args.mac_addr, args.interface)
 	else:
-		print(color("MAC address structure error.", "31"))
+		print(color(f"MAC address {args.mac_addr} structure error.", "31"))
 		sys.exit(1)
 
 if __name__ == "__main__":
