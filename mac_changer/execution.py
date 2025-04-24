997ba32f31ada69d879dc9a5f5e5296f639abb22
youness anflous <anayouness51@gmail.com>
2025-04-24 08:45:10 +0100

    execution

---
+++ mac_changer/execution.py
@@ -0,0 +1,20 @@
+# execution.py
+
+from	mac_changer.parsing import color
+import	subprocess
+import	sys
+import	os
+
+def	check_if_root():
+	if os.geteuid() != 0:
+		print(color("[!] The script must be run as root!!!", "31"))
+		sys.exit(1)
+
+def	setup_new_addr(user_mac_addr, interface):
+	try:
+		subprocess.run(["ip", "link", "set", "dev", interface, "down"], check=True)
+		subprocess.run(["ip", "link", "set", "dev", interface, "address", user_mac_addr], check=True)
+		subprocess.run(["ip", "link", "set", "dev", interface, "up"], check=True)
+	except subprocess.CalledProcessError as error:
+		print(color(f"[-] Error upgrading the MAC address: {error}", "31"))
+		sys.exit(1)
