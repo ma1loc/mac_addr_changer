9674a55dd8d07eb07b348a068217092773f02f07
youness anflous <anayouness51@gmail.com>
2025-04-30 17:51:58 +0100

    parsing

--- mac_changer/parsing.py
+++ mac_changer/parsing.py
@@ -7,9 +7,9 @@
 # ║             ██║ ╚═╝ ██║██║  ██║ ██║███████╗╚██████╔╝╚██████╗             ║
 # ║             ╚═╝     ╚═╝╚═╝  ╚═╝ ╚═╝╚══════╝ ╚═════╝  ╚═════╝             ║
 # ║                                                                          ║
-# ║    Project: mac_addr_changer v1.1.0                                      ║
+# ║    Project: mac_addr_changer v1.1.1                                      ║
 # ║    Created: 2025-02-23                                                   ║
-# ║    Author: ma1loc (youness anflous)                                      ║
+# ║    Author: ma1loc(youness anflous)                                       ║
 # ║                                                                          ║
 # ╚══════════════════════════════════════════════════════════════════════════╝
 
@@ -22,12 +22,9 @@ import	sys
 import	os
 import	re
 
-
 def	user_args():
 	parser = argparse.ArgumentParser(description="MAC Addr changer")
 	parser.add_argument("-i", "--interface", required=True, help="Interface of the mac address option to upgrad its MAC address")
-	
-	# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> group >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 	group = parser.add_mutually_exclusive_group(required=True)
 	group.add_argument("-m", "--mac_addr", help="MAC addr to change the interface MAC address to new one")
 	group.add_argument("--check", action="store_true", help="Check current interface MAC address.")
@@ -43,10 +40,10 @@ def	is_valid_input(interface, mac_addr):
 	if not check_interface_exists(interface):
 		print(color(f"[-] Interface {interface} not exists.", "31"))
 		sys.exit(1)
-
-	if not check_misconfigured_mac(mac_addr):
-		print(color("[!] Invalid MAC address -> (must be unicast).", "31"))
-		sys.exit(1)
+	if (mac_addr):
+		if not check_misconfigured_mac(mac_addr):
+			print(color("[!] Invalid MAC address -> (must be unicast).", "31"))
+			sys.exit(1)
 
 def	parsing_input(user_mac_addr):
 	pattern = r'^([0-9a-fA-F]{2}[:-]){5}([0-9a-fA-F]{2})$'
@@ -81,11 +78,12 @@ def	check_command_exists():
 		print(color(r"[-] The command '{ip}' is not installed. Please install it before running the script."))
 		sys.exit(1)
 
-def check_mac_upgraded(old_mac, new_mac):
+def check_mac_upgraded(old_mac, new_mac, interface):
 	if old_mac != new_mac:
-		print(color("[+] MAC address has been successfully changed!", "92"))
+		print(color(f"[+] MAC address \"{new_mac}\" has been successfully changed on \"{interface}\"", "92"))
 	elif old_mac == new_mac:
 		print(color("[!] No change detected: the new MAC address is the same as the current one.", "33"))
+		sys.exit(1)
 	else:
 		print(color("[-] MAC address couldn't be changed!", "31"))
 		sys.exit(1)
