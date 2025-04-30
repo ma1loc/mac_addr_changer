a14db854dd038dd2cf2fc36dc640ad72c7b41b17
youness anflous <anayouness51@gmail.com>
2025-04-30 17:52:15 +0100

    random gen

--- mac_changer/random_gen.py
+++ mac_changer/random_gen.py
@@ -7,21 +7,29 @@
 # ║             ██║ ╚═╝ ██║██║  ██║ ██║███████╗╚██████╔╝╚██████╗             ║
 # ║             ╚═╝     ╚═╝╚═╝  ╚═╝ ╚═╝╚══════╝ ╚═════╝  ╚═════╝             ║
 # ║                                                                          ║
-# ║    Project: mac_addr_changer v1.1.0                                      ║
+# ║    Project: mac_addr_changer v1.1.1                                      ║
 # ║    Created: 2025-02-23                                                   ║
-# ║    Author: ma1loc (youness anflous)                                      ║
+# ║    Author: ma1loc(youness anflous)                                       ║
 # ║                                                                          ║
 # ╚══════════════════════════════════════════════════════════════════════════╝
 
-# random mac address --anonym future option to add sooner as possible...
-import	random
+# random_gen.py
 
-def	get_random_addr():
-	number = random.randrange(1, 100)
-	print(number)
+import random
 
-def	main():
-	get_random_addr()
+def gen_random_hex():
+    return random.choice('0123456789abcdef')
 
-if __name__ == "__main__":
-	main()
+def get_first_byte():
+    return random.randint(0x00, 0xff) & 0xFE
+
+def converte_to_hex():
+    first_byte = format(get_first_byte(), '02x')
+    return (first_byte)
+
+def get_random_mac():
+    first_byte = converte_to_hex()
+    second_part = [''.join(gen_random_hex() for _ in range(2)) for _ in range(5)]
+    mac_parts = [first_byte] + second_part
+    full_mac_addr = ':'.join(mac_parts)
+    return (full_mac_addr)
